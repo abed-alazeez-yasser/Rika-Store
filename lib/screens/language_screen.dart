c70786cad81d65c6dd1a0581cef89a924656dc02
abed-alazeez-yasser <71096171+abed-alazeez-yasser@users.noreply.github.com>
2022-09-16 23:04:49 +0300

    add commit

--- lib/screens/language_screen.dart
+++ lib/screens/language_screen.dart
@@ -75,43 +75,25 @@ class _LanguageScreenState extends State<LanguageScreen> {
         const SizedBox(
           height: 16,
         ),
+        // AppCard(
+        //   title: const Text('English'),
+        //   value: '1',
+        //   image: Image.asset('assets/images/UK.png'),
+        //   onChanged: (value) {
+        //     setState(() {
+        //       level = value.toString();
+        //     });
+        //   },
+        // ),
         AppCard(
           title: const Text('English'),
-          value: '1',
+          value: '2',
           image: Image.asset('assets/images/UK.png'),
           onChanged: (value) {
             setState(() {
               level = value.toString();
             });
           },
-        ),
-        AppCard(
-          title: const Text('English'),
-          value: '5',
-          image: Image.asset('assets/images/UK.png'),
-          onChanged: (value) {
-            setState(() {
-              level = value.toString();
-            });
-          },
-        ),
-        Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          elevation: 8,
-          child: RadioListTile(
-            groupValue: level,
-            onChanged: (value) {
-              setState(() {
-                level = value.toString();
-              });
-            },
-            value: '2',
-            controlAffinity: ListTileControlAffinity.trailing,
-            secondary: Image.asset('assets/images/UK.png'),
-            title: const Text('English'),
-          ),
         ),
         Card(
           shape: RoundedRectangleBorder(
@@ -127,7 +109,43 @@ class _LanguageScreenState extends State<LanguageScreen> {
             },
             value: '3',
             controlAffinity: ListTileControlAffinity.trailing,
-            secondary: Image.asset('assets/images/UK.png'),
+            secondary: Image.asset('assets/images/ER.png'),
+            title: const Text('English'),
+          ),
+        ),
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          elevation: 8,
+          child: RadioListTile(
+            groupValue: level,
+            onChanged: (value) {
+              setState(() {
+                level = value.toString();
+              });
+            },
+            value: '4',
+            controlAffinity: ListTileControlAffinity.trailing,
+            secondary: Image.asset('assets/images/Ge.png'),
+            title: const Text('English'),
+          ),
+        ),
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          elevation: 8,
+          child: RadioListTile(
+            groupValue: level,
+            onChanged: (value) {
+              setState(() {
+                level = value.toString();
+              });
+            },
+            value: '5',
+            controlAffinity: ListTileControlAffinity.trailing,
+            secondary: Image.asset('assets/images/pr.png'),
             title: const Text('English'),
           ),
         ),
@@ -135,5 +153,3 @@ class _LanguageScreenState extends State<LanguageScreen> {
     ));
   }
 }
-
-
